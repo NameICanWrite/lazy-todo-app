d2f8a6ed3c9e6243dbdfd8dbfddae7ab116d01f2
Vadim <vadimbaranivsky83@gmail.com>
2023-04-27 20:06:11 +0300

    feat: data fetching and part of the styles

--- packages/frontend/src/modules/my-todos/index.tsx
+++ packages/frontend/src/modules/my-todos/index.tsx
@@ -6,10 +6,11 @@ import { ITodo } from '../common/types/todos.type';
 import todoService from '../../service/todos';
 import { useHistory } from 'react-router-dom';
 import { APP_KEYS } from '../common/consts';
+import styled from 'styled-components';
 
 const MyTodosContainer = () => {
   const history = useHistory()
-  const { isLoading, isError, data: todos, error, refetch } = useQuery(["todos"], () =>
+  const { isLoading, isError, data: todos, error, refetch } = useQuery<ITodo[]>(["todos"], () =>
     todoService.getAllTodos()
   )
   const onDeleteTodo = (id: string) => () => {
@@ -26,20 +27,43 @@ const MyTodosContainer = () => {
   useEffect(() => {
     refetch()
   }, [])
-return (
-  <div>
-  <button onClick={() => history.push(APP_KEYS.ROUTER_KEYS.CREATE_TODO)}>Create todo</button>
-  <br />
-  {
-    todos?.map((todo: ITodo) => <Todo 
-      key={todo.id} 
-      todo={todo} 
-      onDelete={onDeleteTodo(todo.id)} 
-      onComplete={onCompleteTodo(todo.id)}
-    />)
-  }
-  </div>
-)
+
+  console.log('todos', todos)
+
+  console.log('loading', isLoading)
+
+  return (
+    <Container>
+      <Button onClick={() => history.push(APP_KEYS.ROUTER_KEYS.CREATE_TODO)}>
+        Create todo
+      </Button>
+      <TodosTable>
+        {todos && todos.map((todo, index) => (
+          <Todo
+            index={index}
+            key={todo.id}
+            todo={todo}
+            onDelete={onDeleteTodo(todo.id)}
+            onComplete={onCompleteTodo(todo.id)}
+          />
+        ))}
+      </TodosTable>
+    </Container>
+  )
 }
 
 export default MyTodosContainer;
+
+export const Container = styled.div`
+  padding: 20px;
+`
+export const Button = styled.button`
+  width: 120px;
+  height: 50px;
+  background-color: transparent;
+  margin-bottom: 20px;
+`
+
+export const TodosTable = styled('div')`
+  border: 2px solid black;
+`
--- packages/frontend/src/modules/my-todos/todo/todo.tsx
+++ packages/frontend/src/modules/my-todos/todo/todo.tsx
@@ -2,24 +2,69 @@ import { useHistory } from "react-router-dom"
 import { ITodo } from "../../common/types/todos.type"
 import { v4 as uuidv4 } from 'uuid'
 import { APP_KEYS } from "../../common/consts"
+import { FC } from "react"
+import styled from "styled-components"
 
-const Todo = ({todo, onDelete, onComplete}: {todo: ITodo, onDelete: any, onComplete: any} ) => {
-  const {name, description, isCompleted, isPrivate} = todo
+export type TodoProps = {
+  todo: ITodo,
+  onComplete: () => void,
+  onDelete: () => void,
+  index: number
+}
+
+const Todo: FC<TodoProps> = ({ todo, onDelete, onComplete, index }) => {
+  const { name, description, isCompleted, isPrivate } = todo
   const history = useHistory()
-  return (
-    <div>
 
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>{isCompleted ? 'Completed': 'Not completed'}</p>
-      <p>{isPrivate ? 'Private': 'Public'}</p>
-      <button onClick={onDelete}>Delete</button>
-      {!todo.isCompleted && <button onClick={onComplete}>Complete</button>}
-      <button onClick={() => history.push(`${APP_KEYS.ROUTER_KEYS.EDIT_TODO}/${todo.id}`)}>Edit</button>
-      <hr />
-      <br /><br /><br />
-    </div>
+  return (
+    <Container index={index}>
+      <Name>{name}</Name>
+      <Description>{description}</Description>
+      <Actions>
+        <p>{isCompleted ? 'Completed' : 'Not completed'}</p>
+        <p>{isPrivate ? 'Private' : 'Public'}</p>
+        <Button onClick={onDelete}>Delete</Button>
+        {!todo.isCompleted &&
+          <Button onClick={onComplete}>Complete</Button>
+        }
+        <Button onClick={() => history.push(`${APP_KEYS.ROUTER_KEYS.EDIT_TODO}/${todo.id}`)}>
+          Edit
+        </Button>
+      </Actions>
+    </Container>
   )
 }
 
 export default Todo
+
+export const Name = styled('h3')`
+  width: 20%;
+  padding-left: 10px;
+  border-right: 2px solid black;
+  display: flex;
+  align-items: center;
+`
+export const Description = styled('div')`  
+  border-color: black;
+  border-right: 2px solid;
+  width: 60%;
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
+`
+export const Container = styled('div')<{index: number}>`
+  display: flex;
+  box-sizing: border-box;
+  border-color: black;
+  background-color: ${props => props.index % 2 ===0 ? '#ececec' : 'white'}
+`
+
+export const Button = styled('button')`
+  background-color: transparent
+`
+
+export const Actions = styled('div')`
+  width: 20%;
+  padding-left: 10px;
+  border-color: black;
+`
